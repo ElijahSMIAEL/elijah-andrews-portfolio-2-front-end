02bbaa9c7838c80692b48404254dd52642d76b9f
Endrews <elijah.smiael@gmail.com>
2022-08-22 14:24:16 -0500

    finish basic portfolio

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -6,9 +6,22 @@ const Contact = () => {
     <main className={styles.container}>
       <h1 className={styles.header}>Contact Me</h1>
       <div className={styles.contactsContainer}>
-        <a className={styles.contactIcon} href="https://github.com/ElijahSMIAEL" target="_blank" rel="noopener noreferrer"><img src="/images/GitHub-Mark.png" alt="github" /></a>
-        <a className={styles.contactIcon} href="https://www.linkedin.com/in/elijahcandrews/" target="_blank" rel="noopener noreferrer"><img src="/images/LinkedInLogo.png" alt="github" /></a>
-      </div>
+        <div className={styles.contact}>
+          <a className={styles.contactIcon} href="https://github.com/ElijahSMIAEL" target="_blank" rel="noopener noreferrer"><img src="/images/GitHub-Mark.png" alt="github" /></a>
+          <h3>GitHub</h3>
+        </div>
+        <div className={styles.contact}>
+          <a className={styles.contactIcon} href="https://www.linkedin.com/in/elijahcandrews/" target="_blank" rel="noopener noreferrer"><img src="/images/LinkedInLogo.png" alt="github" /></a>
+          <h3>LinkedIn</h3>
+        </div>
+        </div>
+        <div className={styles.email}>
+        <a href="mailto:elijah.smiael@gmail.com" className={styles.contactIcon}><img src="/images/EmailLogo.png" alt="Email" /></a>
+        <div className={styles.emailName}>
+        <h2>Email:</h2>
+        <h3>elijah.smiael@gmail.com</h3>
+        </div>
+        </div>
     </main>
   )
 }
--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -3,9 +3,11 @@ import styles from './Resume.module.css'
 const Resume = () => {
 
   return(
-    <>
-    <h1>Resume Component</h1>
-    </>
+    <main>
+      <div className={styles.resumeContainer}>
+      <a className={styles.resume} href="https://docs.google.com/document/d/1EBNw4IOjj9tg1ZdVhO-Cuc-TmvK5Jb4f04-Ea-Z4XB0/edit?usp=sharing" target='_blank' rel="noopener noreferrer" ><img src="/images/ResumeScreenshot.png" alt="Resume" /></a>
+      </div>
+    </main>
   )
 }
 
