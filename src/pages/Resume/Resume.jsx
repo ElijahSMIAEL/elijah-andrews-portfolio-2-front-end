db287e014d0788c631e6827a9b31a797aaa398d8
Endrews <elijah.smiael@gmail.com>
2022-08-22 17:08:24 -0500

    update resume icon

--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -5,7 +5,7 @@ const Resume = () => {
   return(
     <main>
       <div className={styles.resumeContainer}>
-      <a className={styles.resume} href="https://docs.google.com/document/d/1EBNw4IOjj9tg1ZdVhO-Cuc-TmvK5Jb4f04-Ea-Z4XB0/edit?usp=sharing" target='_blank' rel="noopener noreferrer" ><img src="/images/ResumeScreenshot.png" alt="Resume" /></a>
+      <a className={styles.resume} href="https://docs.google.com/document/d/1EBNw4IOjj9tg1ZdVhO-Cuc-TmvK5Jb4f04-Ea-Z4XB0/edit?usp=sharing" target='_blank' rel="noopener noreferrer" ><img src="/images/ECAResume.png" alt="Resume" /></a>
       </div>
     </main>
   )
