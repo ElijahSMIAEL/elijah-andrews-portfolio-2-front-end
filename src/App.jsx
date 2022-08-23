cd3bcd013e25e6d0edf8ed37e3b1df186cf19336
Endrews <elijah.smiael@gmail.com>
2022-08-23 15:46:43 -0500

    move getwork to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Routes, Route, useNavigate, Navigate } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
 import Login from './pages/Login/Login'
 import Landing from './pages/Landing/Landing'
+import * as workService from './services/workService'
 import * as authService from './services/authService'
 import Contact from './pages/Contact/Contact'
 import Resume from './pages/Resume/Resume'
@@ -10,7 +11,30 @@ import Works from './pages/Works/Works'
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser())
+  const [works, setWorks] = useState([])
   const navigate = useNavigate()
+
+  useEffect(() => {
+    const fetchAllWorks = async () => {
+      const workData = await workService.getAll()
+      setWorks(workData)
+    }
+    fetchAllWorks()
+  }, [])
+
+  const handleAddWork = async (workData, photo) => {
+    const newWork = await workService.create(workData)
+    setWorks([...works, newWork])
+    if (photo) {
+      newWork.photo = await workPhotoHelper(photo, newWork._id)
+    }
+  }
+
+  const workPhotoHelper = async (photo, id) => {
+    const photoData = new FormData()
+    photoData.append('photo', photo)
+    return await workService.addPhoto(photoData, id)
+  }
 
   const handleLogout = () => {
     authService.logout()
@@ -42,7 +66,7 @@ const App = () => {
         />
         <Route
           path='/works'
-          element={<Works profile={user?.profile}/>}
+          element={<Works profile={user?.profile} handleAddWork={handleAddWork} works={works}/>}
         />
       </Routes>
     </>
--- src/pages/Works/Works.jsx
+++ src/pages/Works/Works.jsx
@@ -6,39 +6,17 @@ import { useState, useEffect } from 'react'
 
 const Works = (props) => {
   const isOwner = props.profile === '62ed37e4bd59c21ec5b6352e'
-  const [works, setWorks] = useState([])
+  const works = props.works
 
-  useEffect(() => {
-    const fetchAllWorks = async () => {
-      const workData = await workService.getAll()
-      setWorks(workData)
-    }
-    fetchAllWorks()
-  }, [])
-
-  const handleAddWork = async (workData, photo) => {
-    const newWork = await workService.create(workData)
-    setWorks([...works, newWork])
-    if (photo) {
-      newWork.photo = await workPhotoHelper(photo, newWork._id)
-    }
-  }
-
-  const workPhotoHelper = async (photo, id) => {
-    const photoData = new FormData()
-    photoData.append('photo', photo)
-    return await workService.addPhoto(photoData, id)
-  }
-  
   return(
     <>
       <main>
         { isOwner ?
-          <AddWorkForm handleAddWork={handleAddWork}/>
+          <AddWorkForm handleAddWork={props.handleAddWork}/>
           :
           <></>
         }
-          { works ?          
+          { !(works === []) ?          
           <div className={styles.worksContainer}>
           {works.map(work =>
             <WorkCard
@@ -48,7 +26,7 @@ const Works = (props) => {
             )}
           </div>
           :
-          <h1 className={styles.loadMessage}>Loading Works.</h1>
+          <h1 className={styles.loadMessage}>Loading Works...</h1>
         }
       </main>
     </>
