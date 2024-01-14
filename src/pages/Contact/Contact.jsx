a55f5a778f6a4720c87e837a91e4d1a57471d411
Endrews <elijah.smiael@gmail.com>
2024-01-13 18:24:05 -0600

    update styling for different media types

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -4,8 +4,6 @@ const Contact = () => {
 
   return(
     <main className={styles.container}>
-      <h1 className={styles.header}>Contact Me</h1>
-      <div className={styles.contactsContainer}>
         <div className={styles.contact}>
           <a className={styles.contactIcon} href="https://github.com/ElijahSMIAEL" target="_blank" rel="noopener noreferrer"><img src="/images/GitHub-Mark.png" alt="github" /></a>
           <h3>GitHub</h3>
@@ -14,13 +12,9 @@ const Contact = () => {
           <a className={styles.contactIcon} href="https://www.linkedin.com/in/elijahcandrews/" target="_blank" rel="noopener noreferrer"><img src="/images/LinkedInLogo.png" alt="github" /></a>
           <h3>LinkedIn</h3>
         </div>
-        </div>
-        <div className={styles.email}>
+        <div className={styles.contact}>
         <a href="mailto:elijah.smiael@gmail.com" className={styles.contactIcon}><img src="/images/EmailLogo.png" alt="Email" className={styles.emailIcon}/></a>
-        <div className={styles.emailName}>
-        <h2>Email:</h2>
-        <h3>elijah.smiael@gmail.com</h3>
-        </div>
+        <h3>elijah.smiael</h3>
         </div>
     </main>
   )
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -9,7 +9,7 @@ const Landing = () => {
       <h1 className={styles.header}>Elijah Andrews</h1>
       </div>
       <div className={styles.bio}>
-        <h3>I am a developer working mainly with computer hardware and accessories 
+        <h3>I am a Chicago-based developer working mainly with computer hardware and accessories 
         </h3>
         <h3>Developing software provides new challenges that I seek to learn from</h3>
         <h3>I enjoy writing web applications, especially using React, however I will not shy away from unfamiliar territory</h3>
