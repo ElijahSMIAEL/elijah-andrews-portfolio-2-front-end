79922ac2e96624c260ee97ec23bdfa61f25da9d7
Endrews <elijah.smiael@gmail.com>
2024-01-16 18:49:25 -0600

    style changes for contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -6,15 +6,24 @@ const Contact = () => {
     <main className={styles.container}>
         <div className={styles.contact}>
           <a className={styles.contactIcon} href="https://github.com/ElijahSMIAEL" target="_blank" rel="noopener noreferrer"><img src="/images/GitHub-Mark.png" alt="github" /></a>
-          <h3>GitHub</h3>
+          <div className={styles.contactText}>
+            <h3>GitHub</h3>
+            <p>See more of my ongoing code or projects</p>
+          </div>
         </div>
         <div className={styles.contact}>
           <a className={styles.contactIcon} href="https://www.linkedin.com/in/elijahcandrews/" target="_blank" rel="noopener noreferrer"><img src="/images/LinkedInLogo.png" alt="github" /></a>
-          <h3>LinkedIn</h3>
+          <div className={styles.contactText}>
+            <h3>LinkedIn</h3>
+            <p>Follow what I am working on</p>
+          </div>
         </div>
         <div className={styles.contact}>
-        <a href="mailto:elijah.smiael@gmail.com" className={styles.contactIcon}><img src="/images/EmailLogo.png" alt="Email" className={styles.emailIcon}/></a>
-        <h3>elijah.smiael</h3>
+          <a href="mailto:elijah.smiael@gmail.com" className={styles.contactIcon}><img src="/images/EmailLogo.png" alt="Email" className={styles.emailIcon}/></a>
+          <div className={styles.contactText}>
+            <h3>elijah.smiael</h3>
+            <p>Send me an email directly</p>
+          </div>
         </div>
     </main>
   )
